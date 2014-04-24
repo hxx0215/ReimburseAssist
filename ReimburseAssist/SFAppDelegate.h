9fd0cd7b2ac1f0d22d3016da0c30e56fab55f28d
hxx0215 <hxx0215@126.com>
2014-04-24 23:13:21 +0800

    Initial Commit

---
+++ ReimburseAssist/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  ReimburseAssist
+//
+//  Created by hxx on 14-4-24.
+//  Copyright (c) 2014年 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReimburseAssist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReimburseAssist
+//
+//  Created by hxx on 14-4-24.
+//  Copyright (c) 2014年 hxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SFAppDelegate class]));
+    }
+}
